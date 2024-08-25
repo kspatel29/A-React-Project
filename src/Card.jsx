c8640f0dad97b3aba4cf65a0d452df454ae926e7
Kush <kushpatel2627@gmail.com>
2024-08-25 16:37:41 -0600

    Initial commit

---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="https://via.placeholder.com/150" alt="cool image" />
+            <h2>Card</h2>
+            <p>I have designed this page</p>
+        </div>
+    );
+}
+
+export default Card
